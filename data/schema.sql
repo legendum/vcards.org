4dacbdb28557bd66f9d4b8af32aa82689048e9f4
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2015-04-01 21:01:26 -0600

    update the schema

--- data/schema.sql
+++ data/schema.sql
@@ -1,32 +1,37 @@
-create table if not exists servers (
+create table if not exists auths (
     id          bigint unsigned auto_increment primary key,
     user_id     bigint unsigned not null,
-    hostname    varchar(255) not null,
-    description varchar(255),
-    config      mediumtext not null, 
-    created_at  timestamp not null
+    provider    varchar(255),
+    config_pvt  mediumblob not null, 
+    created_at  timestamp not null,
+
+    key         auth_user_id(user_id)
 );
 
 create table if not exists users (
     id          bigint unsigned auto_increment primary key,
-    server_id   bigint unsigned not null,
     card_id     bigint unsigned not null,
     country     char(2) not null,
-    config      mediumtext not null, 
+    config      mediumblob not null, 
+    config_pvt  mediumblob not null, 
     password    varchar(255) not null,
-    public_key  mediumtext not null,
-    private_key mediumtext not null,
-    updated_at  datetime,
-    created_at  timestamp not null
+    public_key  text not null,
+    private_key text not null,
+    created_at  timestamp not null,
+
+    key         user_card_id(card_id)
 );
 
 create table if not exists cardnames (
     id          bigint unsigned auto_increment primary key,
-    server_id   bigint unsigned not null,
     user_id     bigint unsigned not null,
     card_id     bigint unsigned not null,
     cardname    varchar(255),
-    created_at  timestamp not null
+    created_at  timestamp not null,
+
+    key         cardname_user_id(user_id),
+    key         cardname_card_id(card_id),
+    unique key  cardname_cardname(cardname)
 );
 
 create table if not exists subscriptions (
@@ -34,7 +39,10 @@ create table if not exists subscriptions (
     user_id     bigint unsigned not null,
     card_id     bigint unsigned not null,
     state       varchar(255),
-    created_at  timestamp not null
+    created_at  timestamp not null,
+
+    key         subscription_user_id(user_id),
+    key         subscription_card_id(card_id)
 );
 
 create table if not exists cards (
@@ -44,17 +52,28 @@ create table if not exists cards (
     cardname_id bigint unsigned not null,
     longitude   mediumint,
     latitude    mediumint,
+    priority    smallint unsigned,
+    config      mediumblob not null,
+    config_pvt  mediumblob not null, 
     version_at  datetime,
-    created_at  timestamp not null
+    created_at  timestamp not null,
+
+    key         card_user_id(user_id),
+    key         card_cardname_id(cardname_id),
+    key         card_version(version_at)
 );
 
-create table if not exists card_versions {
+create table if not exists card_versions (
     id          bigint unsigned auto_increment primary key,
     card_id     bigint unsigned not null,
-    field_cache mediumblob,
-    field_delta mediumblob,
-    created_at  timestamp not null
-}
+    fields      mediumblob,
+    fields_pvt  mediumblob,
+    is_crypted  char(1),
+    created_at  timestamp not null,
+
+    key         card_version_card_id(card_id),
+    key         card_version_created(created_at)
+);
 
 create table if not exists fields (
     id          smallint unsigned auto_increment primary key,
@@ -69,5 +88,24 @@ create table if not exists field_values (
     value       blob,
     is_private  char(1),
     updated_at  datetime,
-    created_at  timestamp not null
+    created_at  timestamp not null,
+
+    key         field_value_card(card_id),
+    key         field_value_field(field_id)
+);
+
+create table if not exists payments (
+    id          bigint unsigned auto_increment primary key,
+    user_id     bigint unsigned not null,
+    created_at  timestamp not null,
+    currency    char(3),
+    amount      decimal(9,2),
+    account     varchar(255),
+    reference   varchar(255),
+    product     varchar(255),
+    period      varchar(255),
+    coupon      varchar(255),
+
+    key         payment_user(user_id),
+    key         payment_created(created_at)
 );
